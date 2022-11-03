4f768db002ca82bfd4ab8923669f53138f55c9c9
kishankm19 <94464104+kishankm19@users.noreply.github.com>
2022-11-03 17:51:57 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -2,8 +2,10 @@ function validate(){
   if(password == "/robots")
   {
     alert("HAHHAAAA.Tricked you!!");
+    return false;
   else
   {
     alert("Oh hello genius,type what is told!");
+    return false;
   }
   }
