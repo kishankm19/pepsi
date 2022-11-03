d1a745459ab1493bc438537435cd547c8b63f8c0
kishankm19 <94464104+kishankm19@users.noreply.github.com>
2022-11-03 17:59:34 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,4 +1,14 @@
-function validate()
-  {
-    alert("HAHHAAAA.Tricked you!!");
-  }
+function validate(){
+var password = document.getElementById("password").value;
+  if(password == "/robots)
+     {
+         alert("HAHHAAAA.Tricked you!!");
+}
+else
+{
+  alert("ohh genius,type what is said!!");
+}
+}
+
+  
+
